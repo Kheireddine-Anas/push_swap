809f84446e7c65c3526a8d016fc2afca9f980644
Kheireddine-Anas <kheireddine.anas@gmail.com>
2024-05-04 17:58:29 +0100

    Preparint ENV

---
+++ be.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+    int j = 0;
+    int *ints;
+    int spc = 0;
+
+    while (argv[i])
+    {
+        while (argv[i][j])
+        {
+            if (argv[i][j] == ' ' )
+            if (argv[i][j] == ' ')
+                spc++;
+            
+        }
+        
+    }
+    
+}
